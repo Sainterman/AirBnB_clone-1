43a5fe20db82ba18f311e21fd60cfff591bf5da7
Santiago Castillo <sancastillog@gmail.com>
2020-08-18 02:33:21 +0000

    task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Install nginx and configure default pages and routes
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo service nginx start
+
+sudo mkdir -p "/data/web_static/releases/test"
+sudo mkdir -p "/data/web_static/shared/"
+sudo touch "/data/web_static/releases/test/index.html"
+echo "<html><head><title>test</title></head><body><p>just fior testing.</p></body></html>">>/data/web_static/releases/test/index.html
+
+sudo ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
+sudo chown -R ubuntu:ubuntu /data/
+config="/etc/nginx/sites-enabled/default"
+sudo sed -i "/server_name _;/ a \\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n" "$config"
+sudo service nginx restart
