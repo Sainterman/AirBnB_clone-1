5811330d1ed8cebd2c476a3a4d7703eb160f85c2
Santiago Castillo <sancastillog@gmail.com>
2020-08-18 03:19:56 +0000

    task 	1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Ey que pasa amigos de youtube"""
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """Make it bro"""
+    local("mkdir -p versions")
+    catched = local("tar -czvf versions/web_static{}.tar.gz web_static"
+                    .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                    capture=True)
+    if catched.failed:
+        return None
+    return catched
